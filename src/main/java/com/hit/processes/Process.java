bfbea81b92eb0cc5597fb19ef3335b8fc2823f99
ShellyAbadi <30901381+ShellyAbadi@users.noreply.github.com>
2017-09-09 19:59:37 +0300

    process define all class members and implemented the method call()

--- src/main/java/com/hit/processes/Process.java
+++ src/main/java/com/hit/processes/Process.java
@@ -1,27 +1,53 @@
 package com.hit.processes;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import com.hit.memoryunits.MemoryManagementUnit;
+import com.hit.memoryunits.Page;
 
 public class Process implements Callable<Boolean> {
-
+	MemoryManagementUnit mmu;
+	int processId;
+	ProcessCycles processCycles;
+	
 	public Process(int id, MemoryManagementUnit mmu, ProcessCycles processCycles) {
-
+		this.processId = id;
+		this.mmu = mmu;
+		this.processCycles = processCycles;
 	}
 
 	public int getId() {
-		return 0;
+		return processId;
 	}
 
 	public void setId(int id) {
-
+		this.processId = id;
 	}
 
 	@Override
 	public Boolean call() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try
+		{
+			for (ProcessCycle cycle : processCycles.getProcessCycles())
+			{
+				Long[] pagesIds = (Long[]) cycle.getPages().toArray();
+				Page[] pages = mmu.getPages(pagesIds);
+				List<byte[]> data = cycle.getData();
+				int index = 0;
+					for (Page page : pages)
+					{
+						page.setContent(data.get(index));
+						index++;
+					}
+				Thread.sleep(cycle.getSleepMs());
+			}
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+		return true;
 	}
 
 }
